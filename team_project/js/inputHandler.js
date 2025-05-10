97734037833d776c70244ea62cc686efa3297e15
MM-109 <mumzy2002@gmail.com>
2025-05-09 17:09:17 -0700

    Add comments to nputHandler.js

--- team_project/js/inputHandler.js
+++ team_project/js/inputHandler.js
@@ -1,4 +1,9 @@
+// This function processes user input and updates the page with a message
 function processInput() {
+    // Get the value entered by the user in the input field
     const input = document.getElementById('userInput').value;
+
+    // Update the output paragraph with a custom message
     document.getElementById('output').innerText = `Great choice! We love ${input}.`;
 }
+
