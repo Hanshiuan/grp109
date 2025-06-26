d89810f118ea7af1a826665a11aca30945f3c8fd
Harley <harleypettis@pm.me>
2025-06-25 17:56:52 -0700

    Added audio to carousel clicks, and then updated the cart layout

--- team_project/js/carousel.js
+++ team_project/js/carousel.js
@@ -7,24 +7,27 @@ function showSlide(index) {
   });
 }
 
+// HTML uses this to go to next slide when button is clicked
 function nextSlide() {
   currentSlide = (currentSlide + 1) % slides.length;
   showSlide(currentSlide);
 }
 
+// HTML uses this to go to previous slide when button is clicked
 function previousSlide() {
   currentSlide = (currentSlide - 1 + slides.length) % slides.length;
   showSlide(currentSlide);
 }
 
+// Resets the timer when the slides are manually changed to next/previous slides
 function resetSlideInterval() {
   clearInterval(slideInterval);
-  slideInterval = setInterval(nextSlide, 7000);
+  slideInterval = setInterval(nextSlide, 3000);
 }
 
 document.addEventListener("DOMContentLoaded", function () {
   slides = document.querySelectorAll(".slides");
   showSlide(currentSlide);
-  setInterval(nextSlide, 5000); // Change slide every 5 seconds
+  setInterval(nextSlide, 3000); // Change slide every 3 seconds
 });
 
